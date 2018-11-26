012ec51396c221039da8f219848233cca061634d
Morgan <morgan.hamre@gmail.com>
2018-11-26 15:21:46 +0100

    Raycast and lights
    Added a raycast script that adds light to an object when the raycast hits it

---
+++ FinalProject/Assets/scripts/RayCast.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour {
+
+    public GameObject item;
+    public float rayDistance = 20F;
+    public LayerMask layerMask;
+	
+	// Update is called once per frame
+	void Update () {
+        item = checkHit();
+        if(item != null){
+            Light itemLight = item.GetComponent<Light>();
+            itemLight.enabled = true;
+        }
+	}
+
+    GameObject checkHit(){
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, rayDistance, layerMask))
+        {
+            return hit.collider.gameObject;
+        }
+        else{
+            return null;
+        } 
+    }
+
+}
